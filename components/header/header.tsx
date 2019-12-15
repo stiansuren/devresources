08cdb6262bffd445e89c2cdd1d53b3e4e47fd7be
Stian Surén <stian.suren@bekk.no>
2019-12-15 22:25:43 +0100

    Change design and move files

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,7 @@
 import './header-style.scss';
 
 function Header () {
-    return <h1>
-        Dev Resources
-        </h1>
+    return <h1 className="title">Tutorials and tips</h1>
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import { HeadTag } from '../components/head-tag';
-import Header from '../components/header';
-import { Content } from '../components/content';
+import Header from '../components/header/header';
+import { Content } from '../components/content/content';
 import '../components/global-styles.scss';
 
 
