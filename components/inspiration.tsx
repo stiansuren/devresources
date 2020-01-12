96dff683c6dc73a90934b5af81fa0114d5b047bf
Stian Surén <stian.suren@bekk.no>
2020-01-12 13:32:52 +0100

    Add inspiration section on first page

---
+++ components/inspiration.tsx
@@ -0,0 +1,25 @@
+import { Link } from './content/link';
+import './inspiration.scss';
+import { Content } from './content/content';
+
+type Link = {
+    title: string,
+    id: string,
+    url: string,
+    tags: Array<Tags>
+  }
+  
+  type Tags = {
+    tag: string
+  }
+  
+  type InspirationProps = {
+    links: Array<Link>
+  }
+
+export const Inspiration = ({ links } :InspirationProps) => {
+    return <div className="inspiration">
+        <h2 className="inspiration__header">Inspiration</h2>
+        <Content links= {links}/>
+    </div>
+}
